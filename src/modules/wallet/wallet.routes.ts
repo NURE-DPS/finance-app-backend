b88c5ce9487291700fcb5f22bf20fe0f475976a6
Mykhailo Parm <mykhailo.parm.dev@gmail.com>
2025-04-26 19:04:06 +0200

    FTA-45 add body validation

--- src/modules/wallet/wallet.routes.ts
+++ src/modules/wallet/wallet.routes.ts
@@ -1,16 +1,21 @@
-import { PrismaClient } from '@prisma/client';
 import { Router } from 'express';
 import { WalletService } from './wallet.service';
 import { WalletController } from './wallet.controller';
 import { Request, Response } from 'express';
 import prisma from '../../config/prismaClient';
+import { validateBody } from '../../middleware/validateBody';
+import { CREATE_WALLET_SCHEMA } from './wallet.types';
 
 const router = Router();
 const walletService = new WalletService(prisma);
 const controller = new WalletController(walletService);
 
-router.post('/', (req: Request, res: Response) => {
-  controller.create(req, res);
-});
+router.post(
+  '/',
+  validateBody(CREATE_WALLET_SCHEMA),
+  (req: Request, res: Response) => {
+    controller.create(req, res);
+  }
+);
 
 export { router as walletRoutes };
