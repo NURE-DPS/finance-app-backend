03719a96a6c0e72f1ed1b5d22eb942ce25783653
Mykhailo Parm <mykhailo.parm.dev@gmail.com>
2025-04-28 12:09:29 +0200

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -3,27 +3,30 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { walletRoutes } from './modules/wallet/wallet.routes';
 import { authRoutes } from './modules/auth/auth.routes';
-
+import cors from 'cors';
 
 dotenv.config();
-
 
 const app = express();
 const PORT = Number(process.env.PORT) || 8000;
 
-
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: 'http://localhost:5173', // Change this to your frontend URL
+    credentials: true, // Allow cookies & authentication headers
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 
 // Test route
 app.get('/', (req, res) => {
   res.send('Hello world!');
 });
 
-
 app.use('/api/auth', authRoutes);
 app.use('/api/wallets', walletRoutes);
-
 
 app.listen(PORT, () => {
   console.log('started');
