327280ed1830cf014b6366109254126674496d25
Mykhailo Parm <mykhailo.parm.dev@gmail.com>
2025-05-05 22:37:07 +0200

    [FTA-79] feat: add token verification

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -10,6 +10,11 @@ const controller = new AuthController(userRepository);
 
 router.post('/signup', controller.signUp);
 router.post('/signin', controller.signIn);
+router.get('/validate-token', verifyAuth, (req, res) => {
+  res
+    .status(200)
+    .json({ valid: true, user: (req as AuthenticatedRequest).user });
+});
 router.get('/protected', verifyAuth, (req, res) => {
   res.json({
     message: 'You are authenticated',
